131cb951c75286651a88b795c9e9ad3e891e094f
samade747 <samad.e747@gmail.com>
2023-09-03 17:48:29 +0500

    Q55

--- app.js
+++ app.js
@@ -4,11 +4,11 @@
 
 
  // JavaScript code goes here
- function checkAnswer(questionNumber, checkAnswer) {
+ function checkAnswer(questionNumber, rightanswer) {
     var answer = document.querySelector('input[name="q' + questionNumber + '"]:checked');
     var result = document.getElementById('result' + questionNumber);
     
-    if (answer && answer.value === 'c') {
+    if (answer && answer.value === rightanswer) {
         result.textContent = 'Correct!';
     } else {
         result.textContent = 'Incorrect. Try again.';
